e221d1bd9762ed72c6bbe43238d646f6cfd02045
Cary hu <cary.hu@grapecity.com>
2022-07-01 21:01:52 +0800

    feat: setup auto import

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+const props = defineProps<{
+  name: string
+}>();
+const uppercase = computed(() => props.name.toUpperCase());
+</script>
+
 <template>
-  <div>Hello</div>
+  <div>Hello, {{ uppercase }}</div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Component from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Component(),
+    AutoImport({
+      imports: [
+        'vue',
+      ],
+    }),
   ],
 });
