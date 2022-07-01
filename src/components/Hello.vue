f2252a4ad52c9726e482040971a5a91fd66955e0
Cary hu <cary.hu@grapecity.com>
2022-07-01 21:11:54 +0800

    feat: setup unocss

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -6,5 +6,8 @@ const uppercase = computed(() => props.name.toUpperCase());
 </script>
 
 <template>
-  <div>Hello, {{ uppercase }}</div>
+  <div text="green 3xl" class="font-sans p-3">
+    Hello, {{ uppercase }}
+  </div>
+  <div i-carbon-bee text="5xl yellow" />
 </template>
